d73a0095a20c00d829a0e4c208c1da40927e20b5
Steven King <kingsteven1113@gmail.com>
2024-01-13 17:53:11 -0500

    put component in app.js

--- fecapstoneproject/src/App.js
+++ fecapstoneproject/src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from 'react';
 
 
 function App() {
   return (
    <div className='App'>
-      <BrowserRoutes>
+      <BrowserRouter>
         <NavBar>
           <Routes></Routes>
         </NavBar>
-      </BrowserRoutes>
+      </BrowserRouter>
    </div>
   );
 }
