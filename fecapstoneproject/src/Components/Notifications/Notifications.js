1dbcc3d6ed6ac3b71d6c3fecb3d6117b741415d3
Steven King <kingsteven1113@gmail.com>
2024-01-23 20:42:35 -0500

    fixing notifications

--- fecapstoneproject/src/Components/Notifications/Notifications.js
+++ fecapstoneproject/src/Components/Notifications/Notifications.js
@@ -1,31 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import NavBar from '../NavBar/NavBar';
-import './Notifications/Notifications.css';
+import Navbar from '../Navbar/Navbar';
+import "./Notification.css"
 
-const Notifications = ({ children }) => {
-  const open = 'display: block';
-  const closed = 'display: none';
+const Notification = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [doctorData, setDoctorData] = useState(null);
   const [appointmentData, setAppointmentData] = useState(null);
-  const [notificationPanel, setNotificationPanel] = useState(open)
-
-  
 
   useEffect(() => {
     const storedUsername = sessionStorage.getItem('email');
     const storedDoctorData = JSON.parse(localStorage.getItem('doctorData'));
     const storedAppointmentData = JSON.parse(localStorage.getItem(storedDoctorData?.name));
-    
+
     if (storedUsername) {
       setIsLoggedIn(true);
       setUsername(storedUsername);
-
-    }
-    else {
-      setIsLoggedIn(false);
-      setNotificationPanel(closed)
     }
 
     if (storedDoctorData) {
@@ -35,36 +25,45 @@ const Notifications = ({ children }) => {
     if (storedAppointmentData) {
       setAppointmentData(storedAppointmentData);
     }
-    
-    
   }, []);
   return (
     <div>
-      <NavBar ></NavBar>
+      <Navbar ></Navbar>
       {children}
       {isLoggedIn && appointmentData && (
         <>
-          <div className="appointment-card" id='NotificationPanel' style={{notificationPanel}}>
+          <div className="appointment-card">
             <div className="appointment-card__content">
               <h3 className="appointment-card__title">Appointment Details</h3>
               <p className="appointment-card__message">
                 <strong>Doctor:</strong> {doctorData?.name}
               </p>
-              <p className="appointment-card__message"><strong> Specialty</strong> {doctorData.specialty}</p>
-              {appointmentData.map(appointment => (
+
+            <p className="appointment-card__message">
+                                <strong>Speciality:</strong> {doctorData.speciality}
+                            </p>
+                            {appointmentData.map(appointment => (
                                 <div key={appointment.id}>
-              <p className="appointment-card__message"><strong>Appointment Time:</strong>{appointmentData.timeofAppointment}</p>
-              <p className="appointment-card__message"><strong>Appointment Date:</strong>{appointmentData.dateOfAppointment}</p>
-              <p className="appointment-card__message"><strong>Patient Name:</strong>{appointmentData.name}</p>
-              <p className="appointment-card__message"><strong>Patient Phone Number:</strong>{appointmentData.phoneNumber}</p>
-              </div>
-              ))}
-            </div>
-          </div>
-        </>
-      )}
-    </div>
-  );
+                                    <p className="appointment-card__message">
+                                        <strong>Name:</strong> {appointment.name}
+                                    </p>
+                                    <p className="appointment-card__message">
+                                        <strong>Phone Number:</strong> {appointment.phoneNumber}
+                                    </p>
+                                    <p className="appointment-card__message">
+                                        <strong>Date of Appointment:</strong> {appointment.date}
+                                    </p>
+                                    <p className="appointment-card__message">
+                                        <strong>Time Slot:</strong> {appointment.time}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            )}
+        </div>
+    );
 };
 
-export default Notifications;
+export default Notification;
